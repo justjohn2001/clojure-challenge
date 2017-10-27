12624fa8b5c3636ad39fee2a8617064a3ff660cd
John Miller <john.miller@borrowedandblue.com>
2017-10-27 11:49:04 -0400

    solve problem 1 using loop

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,8 +1,8 @@
 (ns euler.core
   (:gen-class))
 
-(defn running-sum [i]
-  (/ (* i (+ i 1)) 2))
+(defn running-sum [n]
+  (/ (* n (+ n 1)) 2))
 
 (defn make-summer [n]
   (fn [i]
@@ -15,6 +15,17 @@
         fiveteens (make-summer 15)]
     (- (+ (threes n) (fives n)) (fiveteens n))))
 
+(defn project1-with-loop [n]
+  (loop [i 1 sum 0]
+    (if (< i n)
+      (if (or (= 0 (mod i 3)) (= 0 (mod i 5)))
+        (recur (inc i) (+ sum i))
+        (recur (inc i) sum))
+      sum)
+    )
+  )
+
 (defn -main
   [& args]
-  (println "Project 1 - " (project1 (- 1000 1))))
+  (println "Project 1 - " (project1 (- 1000 1)))
+  (println "Project 1 using loop - " (project1-with-loop 1000)))
