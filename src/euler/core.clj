6b94094f6cae0f99a7681e6e1a6de85ad2c16f4a
John Miller <john.miller@borrowedandblue.com>
2017-10-27 11:06:58 -0400

    Solve project 1

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,7 +1,20 @@
 (ns euler.core
   (:gen-class))
 
+(defn running-sum [i]
+  (/ (* i (+ i 1)) 2))
+
+(defn make-summer [n]
+  (fn [i]
+    (* (running-sum (Math/floor (/ i n)))
+       n)))
+
+(defn project1 [n]
+  (let [threes (make-summer 3)
+        fives (make-summer 5)
+        fiveteens (make-summer 15)]
+    (- (+ (threes n) (fives n)) (fiveteens n))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Project 1 - " (project1 (- 1000 1))))
